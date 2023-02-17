c7983ea795670f1bd681f94bbdd0861f0a5c526b
TG1899 <86303494+TG1899@users.noreply.github.com>
2023-02-17 10:19:48 +0100

    Add files via upload

---
+++ schueler.lpr
@@ -0,0 +1,16 @@
+program schueler;
+
+uses
+  SysUtils,
+  uSchueler;
+
+var
+                    //Variable meinSchueler initialisieren
+
+begin
+           //meinSchueler erschaffen
+              //Name auf Jonas setzen
+                  //Alter auf 13 setzen
+                      //gruessen aufrufen
+  readln;
+end.
---
+++ uschueler.pas
@@ -0,0 +1,48 @@
+unit uSchueler;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+                          //Erzeugung der Klasse TSchueler
+    private                                       //Festlegung der Attribute als private
+
+
+    public                                        //Festlegung der Methoden als public
+                              //Konstruktor
+                     //Prozeduren und Funktionen (Parameter und Rückgabewert beachten!)
+
+  end;
+
+implementation
+
+                     //Aufruf des Konstuktors
+
+
+
+           //Setzen des Namens
+
+
+
+
+         //Setzen des Alters
+
+
+
+               //Name ermitteln
+
+
+
+           //Alter ermitteln
+
+
+
+                    //Methode zum Grüßen: Der Schüler sagt: 'Guten Tag'
+
+end.
+
+
